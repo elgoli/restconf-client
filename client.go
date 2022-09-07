feb7607553fcb61ca168bebc570c7a85a7f869c5
elgol <>
2022-09-08 00:40:04 +0300

    Add support for creating a loopback interface

--- client.go
+++ client.go
@@ -1,9 +1,40 @@
 package restconf
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
+
+const (
+	IetfInterfacePath = "restconf/data/ietf-interfaces:interfaces"
+	IetfInterfaceType = "iana-if-type:softwareLoopback"
+	contentType       = "application/yang-config+json"
+)
+
+type IetfInterfaceRequest struct {
+	IetfInterface IetfInterface `json:"ietf-interfaces:interface"`
+}
+
+type IetfInterface struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+	Enabled     bool   `json:"enabled"`
+	IPv4        Ipv4   `json:"ietf-ip:ipv4"`
+}
+
+type Ipv4 struct {
+	Address []Address `json:"address"`
+}
+
+type Address struct {
+	Ip      string `json:"ip"`
+	Netmask string `json:"netmask"`
+}
 
 type Client struct {
 	httpClient  *http.Client
@@ -19,4 +50,41 @@ func New(timeout time.Duration, restconfURL string, username string, password st
 		username:    username,
 		password:    password,
 	}
+}
+
+func (c *Client) NewLoopbackInterface(ctx context.Context, req IetfInterfaceRequest) (*http.Response, error) {
+	config, err := json.Marshal(req)
+	if err != nil {
+		return &http.Response{}, fmt.Errorf("failed to encode config %w", err)
+	}
+
+	request, err := c.formRequest(ctx, http.MethodPost, fmt.Sprintf("%s/%s", c.restconfURL, IetfInterfacePath), config)
+	if err != nil {
+		return &http.Response{}, err
+	}
+
+	response, err := c.sendRequest(request)
+	if err != nil {
+		return &http.Response{}, err
+	}
+	return response, nil
+}
+
+func (c *Client) formRequest(ctx context.Context, method string, URI string, config []byte) (*http.Request, error) {
+	request, err := http.NewRequestWithContext(ctx, method, URI, bytes.NewBuffer(config))
+	if err != nil {
+		return nil, fmt.Errorf("failed to form restconf request %w", err)
+	}
+	request.SetBasicAuth(c.username, c.password)
+	request.Header.Add("Content-Type", contentType)
+	request.Header.Add("Accept", contentType)
+	return request, nil
+}
+
+func (c *Client) sendRequest(req *http.Request) (*http.Response, error) {
+	response, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to send restconf request %w", err)
+	}
+	return response, nil
 }
--- client_test.go
+++ client_test.go
@@ -1,7 +1,10 @@
 package restconf
 
 import (
+	"context"
+	"encoding/json"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -21,6 +24,59 @@ func TestNew(t *testing.T) {
 	actualClient := New(30*time.Second, agent.URL, expectedClient.username, expectedClient.password)
 
 	require.Equal(t, expectedClient, actualClient)
+}
+
+func TestNewLoopbackInterface(t *testing.T) {
+	testCase := struct {
+		testName   string
+		httpClient *http.Client
+		username   string
+		password   string
+		config     IetfInterfaceRequest
+	}{
+		testName: "create a loopback interface",
+		username: "username",
+		password: "password",
+		config: IetfInterfaceRequest{IetfInterface: IetfInterface{
+			Name:    "loopback200",
+			Type:    IetfInterfaceType,
+			Enabled: true,
+			IPv4: Ipv4{
+				Address: []Address{{
+					Ip:      "20.0.0.1",
+					Netmask: "255.255.255.255",
+				}}}}},
+	}
+
+	agent := newRestconfAgent()
+	defer agent.Close()
+
+	client := New(30*time.Second, agent.URL, testCase.username, testCase.password)
+	response, err := client.NewLoopbackInterface(context.Background(), testCase.config)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, response)
+	require.Equal(t, http.StatusOK, response.StatusCode)
+}
+
+func TestFormInterfaceConfig(t *testing.T) {
+	config := &IetfInterfaceRequest{IetfInterface: IetfInterface{
+		Name:    "Loopback200",
+		Type:    IetfInterfaceType,
+		Enabled: true,
+		IPv4: Ipv4{
+			Address: []Address{{
+				Ip:      "20.0.0.1",
+				Netmask: "255.255.255.255",
+			}}}}}
+
+	actualConfig, err := json.Marshal(config)
+	require.NoError(t, err)
+
+	expectedConfig, err := ioutil.ReadFile("./test-resources/create-loopback-interface-yang-data.json")
+	require.NoError(t, err)
+
+	require.JSONEq(t, string(expectedConfig), string(actualConfig))
 }
 
 func newRestconfAgent() *httptest.Server {
